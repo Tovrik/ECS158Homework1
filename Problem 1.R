dd2a3763679dea2eb242a3f63fe20307829f332f
Stefan Peterson <stefan.t.peterson@gmail.com>
2015-01-07 17:37:01 -0800

    done

--- Problem 1.R
+++ Problem 1.R
@@ -6,7 +6,7 @@ squaretolist <- function(m)
 {
   apply(m,1,function(m) {
     x1 <- which(m == 1)
-    if(length(x1) == 0) {return "NA"}
+    if(length(x1) == 0) {x1<- "NA"}
     x1
   })
 }
@@ -24,31 +24,47 @@ squaretothin <- function(m)
   }
   tempList
 }
-squaretothin(A)
-#----------------------------------------
-thintolist <- function(thin,nvert) 
-{
-  
-}
-
+C = squaretothin(A)
 #----------------------------------------
 thintosquare <- function(thin,nvert) 
 {
-  
+  tempMatrix <- matrix(0, nrow=nvert, ncol=nvert)
+  tempMatrix[thin] <- 1
+  tempMatrix
 }
+thintosquare(C, 4)
+
+
+#----------------------------------------
+thintolist <- function(thin,nvert) 
+{
+  squaretolist(thintosquare(thin, nvert)) 
+}
+thintolist(C,4)
 
 #----------------------------------------
 listtothin <- function(inlist) 
 {
-  
+  len = length(inlist)
+  tempMatrix <- matrix(, nrow=0, ncol=2)
+  for(i in 1:len) {
+    for(j in inlist[[i]]) {
+      if(j != "NA" ) {
+        tempMatrix <- rbind(tempMatrix, c(i,j))
+      }
+    }
+  }
+  tempMatrix
 }
-
+listtothin(B)
 #----------------------------------------
 listtosquare <- function(inlist,nvert) {
   tempMatrix <- matrix(0, nrow=nvert, ncol=nvert)
   for(i in 1:nvert) {
-    for(j in inlist[i]) {
-      tempMatrix[i,j] <- 1
+    for(j in inlist[[i]]) {
+      if(j != "NA") {
+        tempMatrix[i,j] <- 1
+      }
     }
   }
   tempMatrix
